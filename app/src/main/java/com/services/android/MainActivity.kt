2b48782b411aa260b26076025189ff924d966f90
Karlo Maricevic <you@example.com>
2022-12-04 22:25:09 +0100

    Add stubed method which will open first screen

--- app/src/main/java/com/services/android/MainActivity.kt
+++ app/src/main/java/com/services/android/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.services.android
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 
 class MainActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        openServicePickerScreen()
+    }
+
+    private fun openServicePickerScreen() {
+        setContent {
+            //TODO
+        }
+    }
 }
